11dc03ed025c7c4a890e5a1b1b9338d16cddb62f
ioelion <rughnar@protonmail.com>
2024-12-22 21:25:07 -0300

    SpawnPoints y gamemanager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,61 +1,156 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager INS;
-    public float tiempo = 60;
-    [HideInInspector] public int cantidadObstaculosChocados = 0;
+    [SerializeField] private GameObject loseScreen;
+    [SerializeField] private GameObject winScreen;
+    [SerializeField] private GameObject pauseScreen;
 
-    private UIController uiController;
-    private bool tiempoParado = false;
-    private float coolDownTiempoParado = 5;
+    [SerializeField] private AudioClip winSound;
+    [SerializeField] private AudioClip loseSound;
 
-    public float tiempoDeJuegoReal = 0;
+    [SerializeField] private int currentPlayerAmmo = 5;
+    [SerializeField] private int maxPlayerAmmo = 5;
+    [SerializeField] private int currentPlayerLives = 3;
+    [SerializeField] private int maxPlayerLives = 3;
 
-    private void Awake()
+    [SerializeField] Transform spawnTransform;
+    public KeyCode resetKey = KeyCode.R;
+    public KeyCode pauseKey = KeyCode.P;
+    public KeyCode alternativeResumeKey = KeyCode.Escape;
+    public int firstLevelBuildIndex = 4;
+    private bool gameEnded = false;
+    private bool gamePaused = false;
+    private int sceneIndexToLoadIfReset;
+    private AudioManager audioManager;
+
+    //private AmmoManager ammoManager;
+    //private HPManager hpManager;
+
+    private EnemyManager enemyManager;
+
+
+
+    // Start is called before the first frame update
+    void Start()
     {
-        INS = this;
-        uiController = FindObjectOfType<UIController>();
-        //uiController.SetTimer((int)System.Math.Round(tiempo, 2));
+        Time.timeScale = 1;
+        sceneIndexToLoadIfReset = 1;
+        if (SceneManager.GetActiveScene().buildIndex != firstLevelBuildIndex)
+        {
+            //         playerBehaviourController.SetBullets(PlayerPrefs.GetInt("PlayerBullets"));
+            //         playerBehaviourController.SetHP(PlayerPrefs.GetFloat("PlayerHP"));
+        }
+    }
+
+    void Awake()
+    {
+        //playerMovementController = FindObjectOfType<PlayerMovementController>();
+        //playerBehaviourController = FindObjectOfType<PlayerBehaviourController>();
+        //soundManager = FindObjectOfType<SoundManager>();
+        //ammoManager = FindObjectOfType<AmmoManager>();
+        //ammoManager.SetParameters(currentPlayerAmmo, maxPlayerAmmo);
+        //hpManager = FindObjectOfType<HPManager>();
+        //hpManager.SetParameters(currentPlayerLives, maxPlayerLives);
+        enemyManager = FindObjectOfType<EnemyManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(alternativeResumeKey)) if (gamePaused) Resume();
+
+        if (Input.GetKeyDown(pauseKey))
+        {
+            if (!gamePaused) Pause();
+            else Resume();
+        }
+
+        if (Input.GetKeyDown(resetKey))
+        {
+            if (gameEnded) SceneManager.LoadScene(sceneIndexToLoadIfReset, LoadSceneMode.Single);
+        }
 
     }
 
-    void Update()
+    public void LoseGame()
     {
-        if (tiempo >= 0 && !tiempoParado)
-        {
-            tiempo -= Time.deltaTime;
-            //uiController.SetTimer((int)System.Math.Round(tiempo, 0));
-        }
-        else if (tiempoParado)
-        {
-            //EmpezarCooldownTiempoParado();
+        Time.timeScale = 0;
+        sceneIndexToLoadIfReset = SceneManager.GetActiveScene().buildIndex;
+        StartCoroutine(ShowLoseScreen(1.5f));
+        //soundManager.PlaySFX(loseSound);
 
+    }
+
+    public IEnumerator ShowLoseScreen(float secondsToWait)
+    {
+        yield return new WaitForSecondsRealtime(secondsToWait);
+        loseScreen.SetActive(true);
+        gameEnded = true;
+    }
+
+    public void EndLevel()
+    {
+        if (SceneManager.sceneCountInBuildSettings - 1 != SceneManager.GetActiveScene().buildIndex)
+        {
+            //        PlayerPrefs.SetInt("PlayerBullets", playerBehaviourController.GetBullets());
+            //        PlayerPrefs.SetFloat("PlayerHP", playerBehaviourController.GetHP());
+            //PlayerPrefs.Save();
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
         else
         {
-            SceneManager.LoadScene("Score");
+            WinGame();
         }
-
-        tiempoDeJuegoReal += Time.deltaTime;
-        //Debug.Log(tiempoDeJuegoReal);
     }
 
-    private void OnDisable()
+    public void WinGame()
     {
-        Score();
+        //   playerBehaviourController.Celebrate();
+
+        winScreen.SetActive(true);
+        gameEnded = true;
+        sceneIndexToLoadIfReset = 0;
+        Time.timeScale = 0;
+        //soundManager.PlaySFX(winSound);
     }
 
-
-
-    private void Score()
+    public void Pause()
     {
-        //Formula: (6000 * cantidad de segundos ) - 70 * cantidad de obst�culos chocados
-        string score = ((600 * (int)tiempoDeJuegoReal) - 70 * cantidadObstaculosChocados).ToString();
-        PlayerPrefs.SetString("score", score);
-        PlayerPrefs.Save();
+        Time.timeScale = 0;
+        //    playerMovementController.enabled = false;
+        pauseScreen.SetActive(true);
+        gamePaused = true;
     }
 
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        //    playerMovementController.enabled = true;
+        pauseScreen.SetActive(false);
+        gamePaused = false;
+    }
 
+    public void BackToMainMenu() { SceneManager.LoadScene(0); }
+
+    //public void BulletShot() { ammoManager.ReduceAmmoBy1(); }
+
+    //public void Reload() { ammoManager.Reload(); }
+
+    //public void ReduceLivesByOne() { hpManager.ReduceHPBy1(); }
+
+    //public void RefillLives() { hpManager.Refill(); }
+
+    public int GetCurrentLives() { return currentPlayerLives; }
+
+    public int GetMaxLives() { return maxPlayerLives; }
+
+    public Transform GetSpawnPoint() { return spawnTransform; }
 }
+
